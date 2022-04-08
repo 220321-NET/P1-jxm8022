ac7d18c1c5c619adeee8bb0e511dfa717416cc19
jxm8022 <jose.mendoza5@mavs.uta.edu>
2022-04-08 14:55:15 -0500

    Started Customer Controller

---
+++ TelescopeStoreAPI/Controllers/CustomerController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using BusinessLayer;
+using ModelLayer;
+
+namespace TelescopeStoreAPI.Controllers
+{
+    [Route("api/[controller")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+
+    }
+}
