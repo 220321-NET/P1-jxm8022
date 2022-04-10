e043546c92d993cc30ea19a62346f4059c611736
jxm8022 <jose.mendoza5@mavs.uta.edu>
2022-04-10 13:44:20 -0500

    Added logger; Added get customer request

--- TelescopeStoreAPI/Controllers/CustomerController.cs
+++ TelescopeStoreAPI/Controllers/CustomerController.cs
@@ -4,10 +4,28 @@ using ModelLayer;
 
 namespace TelescopeStoreAPI.Controllers
 {
-    [Route("api/[controller")]
+    [Route("api/[controller]")]
     [ApiController]
     public class CustomerController : ControllerBase
     {
+        private readonly ILogger<CustomerController> _logger;
+        private readonly IBusiness _bl;
 
+        public CustomerController(IBusiness bl, ILogger<CustomerController> logger)
+        {
+            _logger = logger;
+            _bl = bl;
+        }
+
+        [HttpGet("{username}")]
+        public ActionResult<Customer> Get(string username)
+        {
+            Customer customer = _bl.GetCustomer(username);
+            if (customer != null)
+            {
+                return Ok(customer);
+            }
+            return NoContent();
+        }
     }
 }
