cc13fc008fc1d44d885fe559460fb46a00f057c9
jxm8022 <jose.mendoza5@mavs.uta.edu>
2022-04-10 17:30:15 -0500

    Removing DataLayer and BusinessLayer references

--- UILayer/MenuFactory.cs
+++ UILayer/MenuFactory.cs
@@ -6,24 +6,20 @@ public static class MenuFactory
     {
         menu = menu.ToLower();
 
-        string connectionString = File.ReadAllText("./connectionString.txt");
         Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.File("../logs/telescopeLogs.txt").CreateLogger();
-
-        IRepository repo = new DBRepository(connectionString);
-        IBusiness bl = new Business(repo);
 
         switch (menu)
         {
             case "main":
-                return new MainMenu(bl, Log.Logger);
+                return new MainMenu(Log.Logger);
             case "home":
-                return new HomeMenu(bl, Log.Logger);
+                return new HomeMenu(Log.Logger);
             case "manager":
-                return new ManagerMenu(bl, Log.Logger);
+                return new ManagerMenu(Log.Logger);
             case "store":
-                return new StoreMenu(bl, Log.Logger);
+                return new StoreMenu(Log.Logger);
             default:
-                return new MainMenu(bl, Log.Logger);
+                return new MainMenu(Log.Logger);
         }
     }
 }
