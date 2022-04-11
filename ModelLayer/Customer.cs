323b155095c0806ed41de7d2db0161ef2806d177
jxm8022 <jose.mendoza5@mavs.uta.edu>
2022-04-11 16:19:10 -0500

    Changed username property in customer

--- ModelLayer/Customer.cs
+++ ModelLayer/Customer.cs
@@ -2,27 +2,7 @@ namespace ModelLayer;
 
 public class Customer
 {
-    public Customer()
-    {
-        _username = "";
-    }
-
-    public Customer(string username)
-    {
-        _username = username;
-    }
-    private string _username;
-    public string UserName
-    {
-        get
-        {
-            return _username;
-        }
-        set
-        {
-            _username = value;
-        }
-    }
+    public string UserName { get; set; } = "";
     public bool Employee { get; set; } = false;
     public int CustomerID { get; set; } = -1;
     public List<Order> Orders { get; set; } = new List<Order>();
